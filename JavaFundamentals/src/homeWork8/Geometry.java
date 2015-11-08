bcdb478aa3f4c22412f609bffd8a68801871e937
ico <ico@ico-PC>
2015-11-08 18:31:35 +0200

    add coment

---
+++ JavaFundamentals/src/homeWork8/Geometry.java
@@ -0,0 +1,19 @@
+/**
+ * Problem 1.	Geometry
+Define a class structure that models a shape hierarchy.
+	Shape – base class for any kind of shape, holds a list of vertices
+	PlaneShape – base class for all plane (2D) shapes, holds a list of 2D vertices (holding x and y), implements PerimeterMeasurable and AreaMeasurable interfaces
+Triangle – holds 3 vertices
+Rectangle – holds 1 vertex, width, height
+	Circle – holds 1 vertex and radius
+	SpaceShape – base class for all three-dimensional shapes, holds a list of 3D vertices (holding x, y and z), implements AreaMeasurable and VolumeMeasurable interfaces
+	Square Pyramid – holds 1 vertex (base center), base width, pyramid height
+	Cuboid – holds 1 vertex, width, height, depth
+	Sphere – holds 1 vertex and radius
+
+ */
+package homeWork8;
+
+public class Geometry {
+
+}
